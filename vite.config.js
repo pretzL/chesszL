7717d01b17101b0036fe05f9904258e9342782bd
Eric Pretzinger <ericpretzinger@gmail.com>
2024-11-04 09:43:36 +0100

    Add SASS

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+    plugins: [sveltekit()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: '@import "./src/lib/styles/_variables.scss";',
+            },
+        },
+    },
 });
