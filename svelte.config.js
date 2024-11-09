0da2be49bdcebac032663c7e3dbce0f000da2dcc
Eric Pretzinger <ericpretzinger@gmail.com>
2024-11-09 22:57:37 +0100

    Setup barrel export and alias for $components

--- svelte.config.js
+++ svelte.config.js
@@ -5,6 +5,9 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 const config = {
     kit: {
         adapter: adapter(),
+        alias: {
+            $components: "./src/lib/components",
+        },
     },
     preprocess: vitePreprocess(),
 };
