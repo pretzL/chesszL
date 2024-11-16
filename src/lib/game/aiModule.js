4bb838d5c1aa5f561b670d2a93d7abbe9c572727
Eric Pretzinger <ericpretzinger@gmail.com>
2024-11-16 21:21:01 +0100

    Further AI tweaks to make it more aggressive, increase blunder awareness

--- src/lib/game/aiModule.js
+++ src/lib/game/aiModule.js
@@ -7,6 +7,8 @@ export class ChessAI {
         this.color = color;
         this.maxThinkingTime = 3000;
         this.startTime = 0;
+        this.isEarlyGame = true;
+        this.moveCount = 0; 
     }
 
     static PIECE_VALUES = {
@@ -16,6 +18,13 @@ export class ChessAI {
         rook: 500,
         queen: 900,
         king: 20000,
+    };
+
+    static EARLY_GAME_PENALTIES = {
+        queen: -50,
+        rook: -30,
+        bishop: 10,
+        knight: 15
     };
 
     static POSITION_BONUSES = {
@@ -81,6 +90,12 @@ export class ChessAI {
         ],
     };
 
+    static DEVELOPMENT_SQUARES = {
+        knight: [[2, 0], [2, 1], [2, 2], [2, 5], [2, 6], [2, 7]],
+        bishop: [[2, 2], [2, 5], [1, 4], [1, 3]],
+        pawn: [[3, 3], [3, 4], [2, 3], [2, 4]]
+    };
+
     static DIFFICULTY_WEIGHTS = {
         easy: {
             material: 1,
@@ -111,11 +126,11 @@ export class ChessAI {
         let positionalScore = 0;
         let attackScore = 0;
         let centerControlScore = 0;
+        let developmentScore = 0;
+        let safetyScore = 0;
 
-        const centerSquares = [
-            [3, 3], [3, 4],
-            [4, 3], [4, 4]
-        ];
+        const centerSquares = [[3, 3], [3, 4], [4, 3], [4, 4]];
+        const isEarlyGame = this.moveCount < 10;
 
         for (let row = 0; row < 8; row++) {
             for (let col = 0; col < 8; col++) {
@@ -131,13 +146,32 @@ export class ChessAI {
                 const baseValue = ChessAI.PIECE_VALUES[pieceType];
                 materialScore += baseValue * multiplier;
 
+                if (isOurPiece) {
+                    if (isEarlyGame) {
+                        if ((pieceType === 'queen' || pieceType === 'rook') && square.hasMoved) {
+                            developmentScore += ChessAI.EARLY_GAME_PENALTIES[pieceType];
+                        }
+
+                        if ((pieceType === 'knight' || pieceType === 'bishop') && 
+                            ChessAI.DEVELOPMENT_SQUARES[pieceType].some(([r, c]) => r === row && c === col)) {
+                            developmentScore += ChessAI.EARLY_GAME_PENALTIES[pieceType];
+                        }
+
+                        if (pieceType === 'pawn' && 
+                            ChessAI.DEVELOPMENT_SQUARES.pawn.some(([r, c]) => r === row && c === col)) {
+                            developmentScore += 15;
+                        }
+                    }
+
+                    const safety = this.evaluatePieceSafety(row, col, pieceType);
+                    safetyScore += safety * (baseValue / 100);
+
+                    const attacks = this.evaluateAttacks(row, col, pieceType);
+                    attackScore += attacks * (baseValue / 100) * (safety > 0 ? 1.2 : 0.8);
+                }
+
                 const positionBonus = ChessAI.POSITION_BONUSES[pieceType][square.color === "white" ? row : 7 - row][col];
                 positionalScore += positionBonus * multiplier;
-
-                if (isOurPiece) {
-                    const attacks = this.evaluateAttacks(row, col, pieceType);
-                    attackScore += attacks * (baseValue / 100);
-                }
 
                 if (centerSquares.some(([r, c]) => r === row && c === col)) {
                     centerControlScore += baseValue * 0.1 * multiplier;
@@ -149,7 +183,9 @@ export class ChessAI {
         const score = (materialScore * weights.material) +
                      (positionalScore * weights.position) +
                      (attackScore * weights.attack) +
-                     (centerControlScore * weights.center);
+                     (centerControlScore * weights.center) +
+                     (developmentScore * (isEarlyGame ? 1.5 : 0.5)) +
+                     (safetyScore * 0.8);
 
         return isMaximizing ? score : -score;
     }
@@ -175,6 +211,115 @@ export class ChessAI {
         }
 
         return attackScore;
+    }
+
+    evaluatePieceSafety(row, col, pieceType) {
+        let safetyScore = 0;
+        
+        // Get all attackers and defenders of this square
+        const { attackers, defenders } = this.getSquareAttackersAndDefenders(row, col);
+        
+        if (attackers.length > 0) {
+            attackers.sort((a, b) => ChessAI.PIECE_VALUES[a.pieceType] - ChessAI.PIECE_VALUES[b.pieceType]);
+            
+            defenders.sort((a, b) => ChessAI.PIECE_VALUES[a.pieceType] - ChessAI.PIECE_VALUES[b.pieceType]);
+            
+            const exchangeScore = this.evaluateExchangeSequence(
+                pieceType,
+                attackers,
+                defenders
+            );
+            
+            safetyScore += exchangeScore;
+        }
+
+        safetyScore += defenders.length * 10;
+
+        if (pieceType === 'queen' || pieceType === 'rook') {
+            for (const attacker of attackers) {
+                if (ChessAI.PIECE_VALUES[attacker.pieceType] < ChessAI.PIECE_VALUES[pieceType]) {
+                    safetyScore -= 200;
+                }
+            }
+        }
+
+        return safetyScore;
+    }
+
+    isProtectedByOwnPiece(row, col) {
+        for (let r = 0; r < 8; r++) {
+            for (let c = 0; c < 8; c++) {
+                const protector = this.engine.board[r][c];
+                if (protector.piece && protector.color === this.color && 
+                    (r !== row || c !== col)) {
+                    if (this.engine.isValidMove(r, c, row, col, this.color, true)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    getSquareAttackersAndDefenders(row, col) {
+        const attackers = [];
+        const defenders = [];
+        const oppositeColor = this.color === 'white' ? 'black' : 'white';
+
+        for (let r = 0; r < 8; r++) {
+            for (let c = 0; c < 8; c++) {
+                const square = this.engine.board[r][c];
+                if (!square.piece) continue;
+
+                const pieceType = this.engine.getPieceType(square.piece);
+                if (!pieceType) continue;
+
+                if (this.engine.isValidMove(r, c, row, col, square.color, true)) {
+                    const piece = {
+                        row: r,
+                        col: c,
+                        pieceType,
+                        value: ChessAI.PIECE_VALUES[pieceType]
+                    };
+
+                    if (square.color === oppositeColor) {
+                        attackers.push(piece);
+                    } else if (square.color === this.color) {
+                        defenders.push(piece);
+                    }
+                }
+            }
+        }
+
+        return { attackers, defenders };
+    }
+
+    evaluateExchangeSequence(targetPieceType, attackers, defenders) {
+        if (attackers.length === 0) return 0;
+
+        let score = 0;
+        let targetValue = ChessAI.PIECE_VALUES[targetPieceType];
+        
+        let attackerIndex = 0;
+        let defenderIndex = 0;
+        let currentValue = targetValue;
+
+        while (attackerIndex < attackers.length) {
+            score -= currentValue;
+            currentValue = attackers[attackerIndex].value;
+
+            if (defenderIndex < defenders.length) {
+                score += currentValue;
+                currentValue = defenders[defenderIndex].value;
+                defenderIndex++;
+            } else {
+                break;
+            }
+
+            attackerIndex++;
+        }
+
+        return score;
     }
 
     minimax(engine, depth, alpha, beta, isMaximizing) {
@@ -300,8 +445,8 @@ export class ChessAI {
         let bestScore = -Infinity;
 
         moves.sort((a, b) => {
-            const aScore = this.getInitialMoveScore(a);
-            const bScore = this.getInitialMoveScore(b);
+            const aScore = this.getInitialMoveScore(a, this.moveCount < 10);
+            const bScore = this.getInitialMoveScore(b, this.moveCount < 10);
             return bScore - aScore;
         });
 
@@ -324,25 +469,86 @@ export class ChessAI {
             return moves[randomIndex];
         }
 
+        this.moveCount++;
         return bestMove;
     }
 
-    getInitialMoveScore(move) {
+    getInitialMoveScore(move, isEarlyGame) {
         let score = 0;
+        const movingPiece = this.engine.board[move.fromRow][move.fromCol];
+        const pieceType = this.engine.getPieceType(movingPiece.piece);
         const targetSquare = this.engine.board[move.toRow][move.toCol];
-        
-        if (targetSquare.piece) {
-            const targetType = this.engine.getPieceType(targetSquare.piece);
-            if (targetType) {
-                score += ChessAI.PIECE_VALUES[targetType];
+
+        if (isEarlyGame) {
+            if (pieceType === 'queen') score -= 150;
+            if (pieceType === 'rook') score -= 100;
+            if ((pieceType === 'knight' || pieceType === 'bishop') && 
+                ChessAI.DEVELOPMENT_SQUARES[pieceType].some(([r, c]) => r === move.toRow && c === move.toCol)) {
+                score += 50;
             }
+            if (pieceType === 'pawn' && 
+                ChessAI.DEVELOPMENT_SQUARES.pawn.some(([r, c]) => r === move.toRow && c === move.toCol)) {
+                score += 40;
+            }
+        }
+
+        if (targetSquare.piece) {
+            const { attackers, defenders } = this.getSquareAttackersAndDefenders(move.toRow, move.toCol);
+            
+            const postCaptureDefenders = defenders.filter(d => 
+                d.row !== move.fromRow || d.col !== move.fromCol
+            );
+            
+            const exchangeScore = this.evaluateExchangeSequence(
+                pieceType,
+                attackers,
+                postCaptureDefenders
+            );
+
+            if (exchangeScore < -ChessAI.PIECE_VALUES[pieceType] / 2) {
+                score -= 1000;
+            } else {
+                score += exchangeScore * 1.2;
+            }
+
+            if (pieceType === 'queen') {
+                if (attackers.length > 0) {
+                    score -= 500;
+                }
+             
+                if (attackers.some(a => ChessAI.PIECE_VALUES[a.pieceType] < ChessAI.PIECE_VALUES.queen)) {
+                    score -= 1000;
+                }
+            }
+        }
+
+        const potentialAttackers = this.getSquareAttackersAndDefenders(move.toRow, move.toCol).attackers;
+        if (potentialAttackers.length > 0 && 
+            !this.isSquareDefended(move.toRow, move.toCol, move.fromRow, move.fromCol)) {
+            score -= ChessAI.PIECE_VALUES[pieceType];
         }
 
         if ((move.toRow === 3 || move.toRow === 4) && 
             (move.toCol === 3 || move.toCol === 4)) {
-            score += 50;
+            score += 30;
         }
 
         return score;
+    }
+
+    isSquareDefended(row, col, excludeRow, excludeCol) {
+        for (let r = 0; r < 8; r++) {
+            for (let c = 0; c < 8; c++) {
+                if (r === excludeRow && c === excludeCol) continue;
+                
+                const square = this.engine.board[r][c];
+                if (square.piece && square.color === this.color) {
+                    if (this.engine.isValidMove(r, c, row, col, this.color, true)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
